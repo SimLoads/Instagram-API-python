44dd670d552640bc1dfbaa991973d714e50fdc04
pygoose <35179439+SimLoads@users.noreply.github.com>
2019-09-22 16:10:23 +0100

    Update in line with user_followers
    Fix issue caused by requiring the user to enter an id manually, changed to automatic user_id = api.username_id. Also made function based and returns list of users following.

--- examples/user_followings.py
+++ examples/user_followings.py
@@ -7,27 +7,30 @@ from InstagramAPI import InstagramAPI
 import time
 from datetime import datetime
 
-user_id = ''
+def getTotalFollowing(api, user_id):
+    """
+    Returns the list of people the user is following.
+    It should be equivalent of calling api.getTotalFollowings from InstagramAPI
+    """
 
-API = InstagramAPI("login", "password")
-API.login()
+    api.getUsernameInfo(user_id)
+    api.LastJson
+    following = []
+    next_max_id = True
+    while next_max_id:
+        print(next_max_id)
+        # first iteration hack
+        if next_max_id is True:
+            next_max_id = ''
+        _ = api.getUserFollowings(user_id, maxid=next_max_id)
+        following.extend(api.LastJson.get('users', []))
+        next_max_id = api.LastJson.get('next_max_id', '')
+    return following
 
-API.getUsernameInfo(user_id)
-API.LastJson
-following = []
-next_max_id = True
-while next_max_id:
-    print next_max_id
-    # first iteration hack
-    if next_max_id is True:
-        next_max_id = ''
-    _ = API.getUserFollowings(user_id, maxid=next_max_id)
-    following.extend(API.LastJson.get('users', []))
-    next_max_id = API.LastJson.get('next_max_id', '')
-
-len(following)
-unique_following = {
-    f['pk']: f
-    for f in following
-}
-len(unique_following)
+if __name__ == "__main__":
+    api = InstagramAPI("login", "password")
+    api.login()
+    user_id = api.username_id
+    following = getTotalFollowing(api, user_id)
+    for elem in following:
+        print(elem['username'])
